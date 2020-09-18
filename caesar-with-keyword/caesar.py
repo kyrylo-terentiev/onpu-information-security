b8e2577d0ce3f0ba0d9201340c308f1ef76523de
terentyevkirill <kirillterentyev1999@gmail.com>
2020-09-18 19:35:23 +0300

    caesar cipher with keyword. decoding implemented

--- caesar-with-keyword/caesar.py
+++ caesar-with-keyword/caesar.py
@@ -1,25 +1,25 @@
-
-def get_russian_abc():
-    return ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я']
-
-
 def get_english_abc():
     return list(map(chr, range(ord('a'), ord('z') + 1)))
 
 
-def read_encode_input():
-    print('This program performs encoding and decoding of messages using Caesar cipher with a keyword')
+def read_encode_input(abc):
     message = input('Enter message to encode: ')
     key = 0
-    while not 0 < key < len(get_russian_abc()):
+    while not 0 < key < len(abc):
         key = int(input('Enter a desired key (int from 0 to n-1): '))
 
     keyword = input('Enter a desired keyword: ')
     return message, key, keyword
 
 
-def read_decode_input():
+def read_decode_input(abc):
+    encoded = input('Enter encoded message to decode: ')
+    key = 0
+    while not 0 < key < len(abc):
+        key = int(input('Enter a given key (int from 0 to n-1): '))
 
+    keyword = input('Enter a given keyword: ')
+    return encoded, key, keyword
 
 
 def validate(message, keyword, abc):
@@ -32,7 +32,6 @@ def validate(message, keyword, abc):
 
 
 def encode(message, key, keyword, abc, debug):
-    validate(message, keyword, abc)
     # save spaces indices
     spaces_ind = []
     for i in range(0, len(message)):
@@ -90,17 +89,74 @@ def encode(message, key, keyword, abc, debug):
 
 
 def decode(encoded, key, keyword, abc, debug):
-    validate(encoded, keyword, abc)
+    # save spaces indices
+    spaces_ind = []
+    for i in range(0, len(encoded)):
+        if encoded[i] == ' ':
+            spaces_ind.append(i)
+
+    # remove spaces
+    msg = encoded.replace(' ', '')
+    # initialize new alphabet
+    new_abc = ['' for i in range(len(abc))]
+
+    # alphabet without letters from keyword
+    rest_abs = abc.copy()
+    for i in rest_abs:
+        if i in keyword:
+            rest_abs.remove(i)
+
+    # place keyword into new alphabet
+    j = key
+    for i in keyword:
+        new_abc[j] = i
+        j = j + 1
+
+    # fill new alphabe with rest of letters
+    j = 0
+    for i in range(key + len(keyword), len(new_abc)):
+        new_abc[i] = rest_abs[j]
+        j += 1
+
+    for i in range(0, key):
+        new_abc[i] = rest_abs[j]
+        j += 1
+
+    # decode message
+    decoded = ''
+    j = 0
+    for i in msg:
+        if j in spaces_ind:
+            decoded = decoded + ' '
+        index = new_abc.index(i)
+        decoded = decoded + abc[index]
+        j = j + 1
+
+    if debug == 1:
+        print('ENCODE MODE ON')
+        print('-------------------------------------------------------------')
+        print(f'Encoded: {encoded}\nKey: {key}\nKeyword: {keyword}')
+        print(f'Old alphabet:  \t{abc}')
+        print(f'New alphabet:  \t{new_abc}')
+        print(f'Input msg:  \t{encoded}')
+        print(f'Decoded msg: \t{decoded}')
+        print('-------------------------------------------------------------')
+
+    return decoded
 
 
 def main():
+    print('This program performs encoding and decoding of messages using Caesar cipher with a keyword')
     print('Encoding:')
-    message, key, keyword = read_encode_input()
-    encoded_message = encode(message, key, keyword, get_english_abc(), 1)
+    abc = get_english_abc()
+    message, key, keyword = read_encode_input(abc)
+    validate(message, keyword, abc)
+    encoded_message = encode(message, key, keyword, abc, 1)
     print(f'Encoded message: {encoded_message}\n')
 
     print('Decoding:')
-    encoded, key, keyword = read_decode_input()
+    encoded, key, keyword = read_decode_input(abc)
+    validate(encoded, keyword, abc)
     decoded_message = decode(encoded, key, keyword, get_english_abc(), 1)
     print(f'Decoded message: {decoded_message}\n')
 
